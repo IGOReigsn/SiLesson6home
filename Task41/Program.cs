d6be0154d3c98002fb1057ce93073d1558b85543
Igor <eigsn@yandex.ru>
2023-02-06 20:02:23 +0300

    Tasks OK

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -2,3 +2,65 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
+// -------------------МЕТОДЫ----------------------------------------------------------------------------
+int InputNumber(string qwerStr)//проверяет "численность"."Не выпускает" без ввода ЧИСЛА
+    {int num;
+    string? text;
+    while(true)
+        {
+            Console.WriteLine(qwerStr);
+            text=Console.ReadLine();
+           if(int.TryParse(text,out num)) break;
+           Console.WriteLine("Введено некорректное значение.Попробуйте еще раз"); 
+        }
+    return num;  
+    }
+//------------------------------------------------------------------------------------------------------
+int InputNumberWithFilter(string qwerStr, bool zeroEnable, bool negativEnable)//Пропускает положительные. 0 и отрицательные пропускает в соответствии со булевыми значениями zeroEnable,negativEnable
+    {int num;
+    while(true)
+        {
+            num=InputNumber(qwerStr);//проверяет "численность"."Не выпускает" без ввода ЧИСЛА
+           if(num>=0 || num==0 && zeroEnable || num<0 && negativEnable) break;
+           Console.WriteLine("Введено не разрешенное значение.Попробуйте еще раз"); 
+        }
+    return num;  
+        }
+//------------------------------------------------------------------------------------------------------
+int[] InputIntArrayWithSheck(int size)
+{
+int[] array=new int[size];  
+for (int i=0; i<size;i++)
+{
+    Console.Write("Число "+(i+1));
+    array[i]=InputNumber("");
+}
+return array;
+}
+//------------------------------------------------------------------------------------------------------
+int CountPositiveValueInIntArray(int[] array) //Подсчет количества положительных элементов массива array
+{   int countPositive=0;
+    for (int i=0; i < array.Length; i++)
+        {
+           if (array[i]>0) {countPositive++;}
+        }
+return countPositive;
+}
+//------------------------------------------------------------------------------------------------------
+void PrintArrayWithTitle(int[] array,string title,bool sizeView)//Вывод массива с заголовком, указанным в title и отображением длины при sizeView=true
+    {
+        System.Console.Write("Был введен массив");
+        if(sizeView)
+            {System.Console.WriteLine (" из " + array.Length + " элементов:");}
+        else 
+            {System.Console.WriteLine(":");}
+        System.Console.WriteLine("[ "+String.Join(", ",array)+"]");
+    }
+// -------------------ОСНОВНАЯ ПРОГРАММА----------------------------------------------------------------------------
+
+//ЗАДАЧУ МОЖНО РЕШИТЬ БЕЗ МАССИВА - ОБРАБАТЫВАЯ ЧИСЛА ПО МЕРЕ ВВОДА. МАССИВ ДОБАВЛЕН С ЦЕЛЬЮ РАСПЕЧАТКИ (ЧТО НЕОБЯЗАТЕЛЬНО) И ПРОЧИМИ МЕТОДИЧЕСКИМИ ЦЕЛЯМИ
+int m=InputNumberWithFilter("Введите число М -количество чисел, которые надо ввести:", false, false);
+int[] arr=new int[m] ;
+arr=InputIntArrayWithSheck(m);
+PrintArrayWithTitle(arr,"Был введен массив ",true);//Вывод массива с заголовком, указанным в title и отображением длины при sizeView=true
+System.Console.WriteLine("Количество положительных чисел во введенном массиве: "+CountPositiveValueInIntArray(arr));
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,26 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// -------------------МЕТОДЫ----------------------------------------------------------------------------
+double InputNumberDouble(string qwerStr)//проверяет "численность"."Не выпускает" без ввода ЧИСЛА
+    {double num;
+    string? text;
+    while(true)
+        {
+            Console.WriteLine(qwerStr);
+            text=Console.ReadLine();
+           if(double.TryParse(text,out num)) break;
+           Console.WriteLine("Введено некорректное значение.Попробуйте еще раз"); 
+        }
+    return num;  
+    }
+//------------------------------------------------------------------------------------------------------
+// -------------------ОСНОВНАЯ ПРОГРАММА----------------------------------------------------------------------------
+double b1=InputNumberDouble("Введите B1:");
+double k1=InputNumberDouble("Введите К1:");
+double b2=InputNumberDouble("Введите B2:");
+double k2=InputNumberDouble("Введите К2:");
+
+double x=(b2-b1)/(k1-k2);
+double y=k1*x+b1;
+Console.WriteLine($"Решение: Х={Math.Round(x,3)} Y= {Math.Round(y,3)}");
